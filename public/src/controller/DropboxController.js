71e320fde8451002ae9e2cddc59a5aaa1e0bdc1c
Romário Freitas <romarioaf1989@gmail.com>
2019-04-12 22:47:43 -0300

    Adding formidable to get files on request and added endpoint for upload request

--- public/src/controller/DropboxController.js
+++ public/src/controller/DropboxController.js
@@ -18,7 +18,42 @@ class DropboxController {
 
         this.inputFileE1.addEventListener('change', event => {
             this.snackModalE1.style.display = 'block';
+            this.uploadTask(event.target.files);
         });
 
+    }
+
+    uploadTask(files) {
+
+        let promises = [];
+
+        [...files].forEach(file => {
+            promises.push(new Promise((resolve, reject) => {
+
+                let ajax = new XMLHttpRequest();
+
+                ajax.open('POST', '/upload');
+
+                ajax.onload = event => {
+                    try {
+                        resolve(JSON.parse(ajax.responseText))
+                    } catch (e) {
+                        reject(e)
+                    }
+                };
+
+                ajax.onerror = event => {
+                    reject(e);
+                };
+
+                let formData = new FormData();
+                formData.append('input-file', file);
+
+                ajax.send(formData);
+
+            }));
+        })
+
+        return Promise.all(promises);
     }
 }
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,23 @@
 var express = require('express');
+var formidable = require('formidable');//It's a module for parsing form data, specially file uploads
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+router.post('/upload', (req, res) => {
+
+  let form = new formidable.IncomingForm({
+      uploadDir: './upload',
+      keepExtensions: true
+  });
+
+  form.parse(req, (err, fields, files) => {
+    res.json(files)
+  });
+
+})
 
 module.exports = router;
